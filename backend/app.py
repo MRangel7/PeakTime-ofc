1ee585b5ba5c5bc9ec3fdee5d4003a8db742732b
Oalu04 <joaoluismml@gmail.com>
2025-11-25 15:08:01 -0300

    Adicionado nova tabela contendo pessoas por horários e adicionado o apscheduler

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -27,13 +30,16 @@ def register():
     name = data.get("name")
     email = data.get("email")
     phone = data.get("phone")
-    password = data.get("password")  # Em produção -> criptografar!
+    password = data.get("password")  
 
 
     existing_user = User.query.filter_by(email=email).first()
+    existing_phone = User.query.filter_by(phone=phone).first()
     if existing_user:
         return jsonify({"error": "Email já cadastrado"}), 400
-
+    elif existing_phone:
+        return jsonify({"error": "Telefone já cadastrado"}), 400
+    
     new_user = User(
         name=name,
         email=email,
@@ -44,7 +50,7 @@ def register():
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({"message": "Usuário cadastrado com sucesso"}), 201
+    return jsonify({"message": "Usuário cadastrado com sucesso"}), 201 
 
 
 @app.post("/login")
@@ -72,6 +78,71 @@ def login():
         }
     })
 
+# Tabela contendo quantidade de pessoas por horário
+class PeopleCount(db.Model):
+    __tablename__ = "people_count"
+
+    id = db.Column(db.Integer, primary_key=True)
+    hour = db.Column(db.String(5), nullable=False)  
+    total_people = db.Column(db.Integer, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.post("/count")
+def save_people_count():
+    data = request.get_json()
+
+    hour = data.get("hour")
+    total = data.get("total")
+
+    new_count = PeopleCount(
+        hour=hour,
+        total_people=total
+    )
+
+    db.session.add(new_count)
+    db.session.commit()
+
+    return jsonify({"message": "Registro salvo com sucesso"}), 201
+
+@app.get("/counts")
+def get_counts():
+    counts = PeopleCount.query.all()
+
+    result = [
+        {
+            "hora": c.hour,
+            "pessoas": c.total_people
+        } for c in counts
+    ]
+
+    return jsonify(result)
+
+
+
+def automatic_hourly_count():
+    now = datetime.now()
+    hour = now.strftime("%H:00")
+
+    
+    total_people = random.randint(0, 100)  
+
+    new_count = PeopleCount(
+        hour=hour,
+        total_people=total_people
+    )
+
+    with app.app_context():
+        db.session.add(new_count)
+        db.session.commit()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(automatic_hourly_count, 'interval', hours=1)
+scheduler.start()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
